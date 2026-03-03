0122dc96a1620a18b74f20dec3ac26aede8564c3
Joshua Noronha <jnoronha@wisc.edu>
2026-03-03 11:29:53 -0600

    Started driver.v

--- minilab3/driver.v
+++ minilab3/driver.v
@@ -29,5 +29,28 @@ module driver(
     inout [7:0] databus
     );
 
+    case (br_cfg):
+    2'b00: begin
+        assign ioaddr = 2'b10; // DIV_LOW
+        assign databus = 8'd651; // divisor low byte
+    end
+    2'b01: begin
+        assign ioaddr = 2'b10; // DIV_LOW
+        assign databus = 8'd325; // divisor low byte
+    end
+    2'b10: begin
+        assign ioaddr = 2'b10; // DIV_LOW
+        assign databus = 8'd162; // divisor low byte
+    end
+    2'b11: begin
+        assign ioaddr = 2'b10; // DIV_LOW
+        assign databus = 8'd81; // divisor low byte
+    end
+    default: begin
+        assign ioaddr = 2'b00; // SPART
+        assign databus = 8'bz; // high impedance
+    end
+    endcase
+
 
 endmodule
